fd1912c36b902a36f1309854421adf90d1893893
Maurice Consultant <mainna62@outlook.com>
2026-07-17 12:51:09 +0300

    06: session management and config

--- packages/cli/src/components/spinner.tsx
+++ packages/cli/src/components/spinner.tsx
@@ -1,7 +1,13 @@
 import "opentui-spinner/react";
 import { useTheme } from "../providers/theme";
+import { Mode } from "@maxintel/database/enums";
 
-export function Spinner() {
+type Props = {
+    mode?: Mode;
+}
+
+export function Spinner({mode = Mode.BUILD}: Props) {
     const { colors } = useTheme();
-    return<spinner name="aesthetic" color={colors.primary}/>
+    const activeColor = mode === Mode.PLAN ? colors.planMode : colors.primary;
+    return<spinner name="aesthetic" color={activeColor}/>
 }
--- packages/cli/src/components/status-bar.tsx
+++ packages/cli/src/components/status-bar.tsx
@@ -1,13 +1,20 @@
 import { TextAttributes } from "@opentui/core";
 import { useTheme } from "../providers/theme";
+import { usePromptConfig } from "../providers/prompt-config";
+import { Mode } from "@maxintel/database/enums";
 
 export function StatusBar() {
-    const { colors } = useTheme();
+  const { mode, model } = usePromptConfig();
+  const { colors } = useTheme();
   return (
-      <box flexDirection="row" gap={1}>   
-          <text fg={colors.primary}>Build</text>
-          <text attributes={TextAttributes.DIM} fg={"gray"}>{ colors.dimSeparator}</text>
-          <text>Maxintel</text>
-        </box>    
-    );
+    <box flexDirection="row" gap={1}>
+      <text fg={mode === Mode.PLAN ? colors.planMode : colors.primary}>
+        {mode === Mode.PLAN ? "Plan" : "Build"}
+      </text>
+      <text attributes={TextAttributes.DIM} fg={"gray"}>
+        {colors.dimSeparator}
+      </text>
+      <text>{model}</text>
+    </box>
+  );
 }
